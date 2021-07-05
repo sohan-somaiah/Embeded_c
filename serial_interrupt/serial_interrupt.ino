fc24a429e8109a201acc5ad3157700f5d9c8a9b3
sohan-somaiah <80321262+sohan-somaiah@users.noreply.github.com>
2021-07-05 12:37:35 +0530

    Add files via upload

---
+++ serial_interrupt/serial_interrupt.ino
@@ -0,0 +1,21 @@
+byte rx_byte;
+void setup() {
+  // put your setup code here, to run once:
+  // 8bit data format
+  UBRR0H=0x00;
+  UBRR0L=103;
+
+  UCSR0B=(1<<TXEN0)|(1<<RXEN0)|(1<<RXCIE0);
+  UCSR0C=(1<<UCSZ00)|(1<<UCSZ01);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+ISR(USART_RX_vect)
+{
+  rx_byte = UDR0;
+  UDR0 = rx_byte;
+}
